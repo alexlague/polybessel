785ddc55bb233584f9667767153a1822ba91fadf
Alex Laguë <alexlague@Alexs-MacBook-Pro-6.local>
2026-09-14 16:41:34 +0100

    added full single Bessel example

--- src/polybessel.py
+++ src/polybessel.py
@@ -5,32 +5,39 @@
 
 import numpy as np
 from scipy.special import eval_legendre, spherical_jn
-from numba import jit, vectorize
+from numba import jit, vectorize, set_num_threads
 from joblib import Parallel, delayed
 
-from integration import clenshaw_curtis_any_interval #filon_sin_quad, filon_cos_quad
+#from integration import clenshaw_curtis_any_interval #filon_sin_quad, filon_cos_quad
 from interpolation import interp_jl
+from single_bessel_recurrence import precompute_dI_eq33, hankel_transform_multi_l_eq33
 
 class PolyBessel:
-    def __init__(self, x, l, freqs, interp_spacing=0.03, nproc=4, xpad=0.7, M=20):
-        self.lmax = lmax
-        self.N = N
-        self.k_array = k_array
-        self.kp_array = kp_array
-        self.xmin = xmin
-        self.xmax = xmax
-        self.interp_spacing = interp_spacing
-        #self.npts_leg = npts_leg
-        self.nproc = nproc
-        self.xpad = xpad
-
-        self.kmin = np.min([np.min(self.k_array), np.min(self.kp_array)])
-        self.kmax = np.max([np.max(self.k_array), np.max(self.kp_array)])
+    def __init__(self, xs, ls, freqs, interp_spacing=0.03, n_proc=4, x_pad=0.7):
+        """
+        """
+        self.xs = xs
+        self.ls = ls
+        self.freqs = freqs
         
+        self.x_min = np.min(xs)
+        self.x_max = np.max(xs)
+        self.l_min = np.min(ls)
+        self.l_max = np.max(ls)
+        self.freq_min = np.min(freqs)
+        self.freq_max = np.max(freqs)
+
+        self.interp_spacing = interp_spacing
+        self.x_pad = x_pad
+        
+        self.n_proc = n_proc
+        set_num_threads(n_proc)
         # Creating reference arrays
-        self.pre_compute_jl()
+        # skipping for now...
+        #self.pre_compute_jl()
         #self.pre_compute_trig_int()
         #self.pre_compute_Pl()
+        self.dI0, self.dI1 = precompute_dI_eq33(self.xs, self.freqs, self.ls)
 
 
     ### Table Functions ###
@@ -38,21 +45,21 @@ class PolyBessel:
     def pre_compute_jl(self):
         """
         """
-        lower_lim = self.kmin * self.xmin * (1-self.xpad)
-        upper_lim = self.kmax * self.xmax * (1+self.xpad)
+        lower_lim = self.freq_min * self.x_min * (1-self.x_pad)
+        upper_lim = self.freq_max * self.x_max * (1+self.x_pad)
 
         self.npts_sb = int((upper_lim-lower_lim)/self.interp_spacing)
         
-        self.xinterp = np.geomspace(lower_lim, upper_lim, self.npts_sb)
+        self.x_interp = np.geomspace(lower_lim, upper_lim, self.npts_sb)
         
-        self.jl_table = Parallel(n_jobs=self.nproc)(delayed(spherical_jn)(l, self.xinterp) for l in range(self.lmax+2))
+        self.jl_table = Parallel(n_jobs=self.nproc)(delayed(spherical_jn)(l, self.x_interp) for l in range(self.l_max+2))
         self.jl_table = np.array(self.jl_table)
         
         return
 
     def pre_compute_Pl(self):
         """
-        MAY BE NEEDED ONLY FOR LMAX
+        WILL BE USEFUL WHEN CHANGING ENDPOINTS
         """
         #self.leg_x, self.leg_w = np.polynomial.legendre.leggauss(self.npts_leg)
         #self.Pl_interp = Parallel(n_jobs=nproc)(delayed(eval_legendre)(l, self.leg_x) for l in range(self.lmax+self.l_pad+1))
@@ -61,35 +68,32 @@ class PolyBessel:
         N = self.npts_leg
         j = np.arange(N + 1)
         self.leg_x = np.cos(np.pi * j / N)
-        self.Pl_at_lmax = eval_legendre(self.lmax, self.leg_x)
+        self.Pl_at_l_max = eval_legendre(self.l_max, self.leg_x)
         
         return
 
-    def pre_compute_trig_int(self):
-        """
-        For the integrals of x^n sin(ax) or x^n cos(ax)
-        CAN BE DONE ANALYTICALLY
-        """
-        
-        self.trig_alpha_array = np.geomspace(0.2*self.kmin, 5*self.kmax, 100_000) # ADD THIS AS PARAMETER
-        a = self.xmin
-        b = self.xmax
-        o = 300
-        omega = self.trig_alpha_array
-        
-        self.sin_int_interp = Parallel(n_jobs=self.nproc)(delayed(filon_sin_quad)(n, a, b, omega, o) for n in range(self.N+1))
-        self.cos_int_interp = Parallel(n_jobs=self.nproc)(delayed(filon_cos_quad)(n, a, b, omega, o) for n in range(self.N+1))
-
-        self.sin_int_interp_neg1 = filon_sin_quad(-1, a, b, omega, o)
-        self.cos_int_interp_neg1 = filon_cos_quad(-1, a, b, omega, o)
-        
-        return
-
-    ### Interpolation Functions ###
-
-    #MOVED
+    
     
     ### Integration Functions ###
+    def integrate_single_bessel(self, fx):
+        """
+        Compute the integral \int_a^b f(x) j_\ell(\omega x) dx
+        for omega in frequencies array and l values in list.
+        The endpoints are determined by the endpoints of the x array.
+        
+        The first evaluation runs table computation scripts and compilation
+        of numba functions
+
+        input: fx array of same shape as x
+
+        output: array of shape (len(l), len(freqs))
+        """
+            
+        
+        return hankel_transform_multi_l_eq33(self.xs, fx, self.freqs, self.ls, self.dI0, self.dI1)
+
+
+    
     '''
     def K_rec_sph_terms(self, n, l, x, k, kprime):
         """
@@ -113,7 +117,7 @@ class PolyBessel:
         out += (self.K_rec_sph_terms(n, l, xmax, k, kprime) - self.K_rec_sph_terms(n, l, xmin, k, kprime))
         
         return out
-    '''
+    
     @vectorize
     def compute_K_rec(self, k, kprime):
         """
@@ -132,3 +136,4 @@ class PolyBessel:
                 K_integrals[li-1][nj] = (K_integrals[li][nj] - inhom) / b
 
         return
+    '''
